a93b70fa8b2dadc2b0c0520432341b9786498f93
fmunoze <fmunoze@unal.edu.co>
2022-09-26 00:39:22 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -11,7 +11,7 @@ class Animal:
         self._habitat = habitat
         self._genero = genero
         self._zona = None
-        _totalAnimales+=1
+        Animal._totalAnimales+=1
 
     def movimiento(self):
         return "desplazarse"
