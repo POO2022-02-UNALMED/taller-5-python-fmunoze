76d0cfd77996b66602e7f5fbadae4094a2c5f67e
fmunoze <fmunoze@unal.edu.co>
2022-09-26 00:55:28 -0500

    Update ave.py

--- zooAnimales/ave.py
+++ zooAnimales/ave.py
@@ -1,38 +1,42 @@
 from zooAnimales.animal import Animal
 
 class Ave(Animal):
-
-    halcones=0
-    aguilas=0
-    _listado=[]
-
-    def __init__(self,nombre=None,edad=0,habitat=None,genero=None,colorPlumas=None):
-        super().__init__(nombre=nombre,edad=edad,genero=genero)
-        self._colorPlumas=colorPlumas
-        Pez._listado.append(self)
+    _listado = []
+    halcones = 0
+    aguilas = 0
     
-    def movimiento(self):
-        return "volar"
-    @classmethod
-    def crearHalcon(cls,nombre,edad,genero):
-        cls.halcones+=1
-        return Ave(nombre,edad,"montañas",genero,"cafe glorioso")
-    @classmethod
-    def crearAguila(cls,nombre,edad,genero):
-        cls.aguilas+=1
-        return Aguila(nombre,edad,"montañas",genero,"blanco y amarillo")
+    def __init__(self, nombre, edad, habitat, genero, colorPlumas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPlumas = colorPlumas
+        Ave._listado.append(self)
+    
     @classmethod
     def cantidadAves(cls):
         return len(cls._listado)
-    
-    def getColorPlumas(self):
-        return self._colorPlumas
-    def setColorPlumas(self, colorPlumas):
-        self._colorPlumas = colorPlumas
-    
+
+    def movimiento():
+        return ""
+
+    @classmethod
+    def crearHalcon(cls,nombre, edad, genero):
+        cls.halcones += 1
+        return Ave(nombre, edad, "montanas", genero, "cafe glorioso")
+
+    @classmethod
+    def crearAguila(cls,nombre, edad, genero):
+        cls.aguilas += 1
+        return Ave(nombre, edad, "montanas", genero, "blanco y amarillo")
+
     @classmethod
     def getListado(cls):
         return cls._listado
+
     @classmethod
-    def setListado(cls, listado):
+    def setNombre(cls, listado):
         cls._listado = listado
+
+    def getColorPlumas(self):
+        return self._colorPlumas
+
+    def setColorPlumas(self, colorPlumas):
+        self._colorPlumas = colorPlumas
