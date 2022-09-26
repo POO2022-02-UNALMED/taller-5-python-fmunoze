428c4e00b0b83923e5def9e1ad67b10f54f8f540
fmunoze <fmunoze@unal.edu.co>
2022-09-26 00:49:38 -0500

    Update pez.py

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -1,47 +1,49 @@
-from zooAnimales.animal import Animal
 
-class Pez(Animal):
-    _listado=[]
-    salmones=0
-    bacalaos=0
+import zooAnimales.animal
+
+
+class Pez(zooAnimales.animal.Animal):
+    _listado = None
+    salmones = 0
+    bacalaos = 0
+
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorEscamas=colorEscamas
-        self._cantidadAletas=cantidadAletas
-        Pez._listado.append(self)
-    
-    def movimiento():
-        return "nadar"
+        super().__init__(nombre, edad, habitat, genero, self)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
 
-    
-    @classmethod
-    def crearSalmon(cls, nombre, edad, genero):
-        Pez.salmones+=1
-        return Pez(nombre, edad, "oceano", genero, "rojo", 6)
-        
-    @classmethod
-    def crearBacalao(cls,nombre, edad, genero):
-        Pez.bacalaos+=1
-        return Pez(nombre, edad, "oceano", genero, "gris", 6)
-    
-    @classmethod
-    def cantidadPeces(cls):
-        return len(Pez._listado)
-    
-    def getListado(cls):
-        return Pez._listado
-    
-    def setListado(cls, listado):
-        Pez._listado= listado
-    
-    def setColorEscamas(self, color):
-        self._colorEscamas= color
+        if self._listado is None:
+            self._listado = [self]
+        else:
+            self._listado.append(self)
+
+    def getListado(self):
+        return self._listado
+
+    def setListado(self, listado):
+        self._listado = listado
 
     def getColorEscamas(self):
         return self._colorEscamas
-    
-    def setCantidadAletas(self, aletas):
-        self._cantidadAletas= aletas
+
+    def setColorEscamas(self, color):
+        self._colorEscamas = color
 
     def getCantidadAletas(self):
         return self._cantidadAletas
+
+    def setCantidadAletas(self, cantidad):
+        self._cantidadAletas = cantidad
+
+    def cantidadPeces(self):
+        return len(self.getListado())
+
+    @staticmethod
+    def crearSalmon(nombre, edad, genero):
+        Pez.salmones += 1
+        return Pez(nombre, edad, "oceano", genero, "rojo", 6)
+
+    @staticmethod
+    def crearBacalao(nombre, edad, genero):
+        Pez.bacalaos += 1
+        return Pez(nombre, edad, "oceano", genero, "gris", 6)
